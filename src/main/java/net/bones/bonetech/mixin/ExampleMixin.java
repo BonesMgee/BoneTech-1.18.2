b89a956346b0d5cc0b98b73489b63ae5ed6f8136
BonesMgee <111199847+BonesMgee@users.noreply.github.com>
2022-10-29 02:14:25 +0100

    Initial commit

---
+++ src/main/java/net/bones/bonetech/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.bones.bonetech.mixin;
+
+import net.bones.bonetech.BoneTech;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		BoneTech.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
